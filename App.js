0742ddc3391f2386262e3b52cc03f1a9d0107993
Hy-M <humayraa.m@outlook.com>
2020-10-07 11:59:42 +0100

    access phone location

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as Location from "expo-location";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [errorMessage, setErrorMessage] = useState(null);
+
+	useEffect(() => {
+		load();
+	}, []);
+
+	async function load() {
+		try {
+			let { status } = await Location.requestPermissionsAsync();
+
+			if (status !== "granted") {
+				setErrorMessage("Location access is needed to run this app.");
+				return;
+			}
+
+			const location = await Location.getCurrentPositionAsync();
+			const { latitude, longitude } = location.coords;
+			alert(`${latitude} ${longitude}!!`);
+		} catch (error) {}
+	}
+
+	return (
+		<View style={styles.container}>
+			<Text>Hello world</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center"
+	}
 });
